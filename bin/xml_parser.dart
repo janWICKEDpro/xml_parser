195f63c08b618e20fb595daebbfdfa5ddc391ae6
janWICKEDpro <janroyal45@gmail.com>
2023-05-31 14:09:46 +0100

    printing out the elements

--- bin/xml_parser.dart
+++ bin/xml_parser.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 import 'package:xml_parser/content_handler.dart';
-import 'package:xml_parser/xml_parser.dart';
+import 'package:xml_parser/sax_parser.dart';
+import 'package:xml_parser/xml_element.dart';
 
 void main() {
   //read the file
@@ -29,9 +30,37 @@ void main() {
     print('The XML document is valid.');
     print('List of elements:');
     for (var element in contentHandler.elements) {
-      print(element.name);
+      printXmlElement(element, "\t");
     }
   } catch (e) {
     print('The XML document is invalid: ${e.toString()}');
   }
+}
+
+void printXmlElement(XmlElement element, [String prefix = '']) {
+  print('$prefix${element.name}${_formatAttributes(element.attributes)}');
+
+  if (element.text.isNotEmpty) {
+    print('$prefix  ${element.text}');
+  }
+
+  if (element.children != null) {
+    for (var child in element.children!) {
+      printXmlElement(child, '$prefix  ');
+    }
+  }
+
+  print('$prefix${element.name}');
+}
+
+String _formatAttributes(Map<String, String> attributes) {
+  if (attributes.isEmpty) return '';
+
+  final List<String> formattedAttributes = [];
+
+  attributes.forEach((key, value) {
+    formattedAttributes.add('$key="$value"');
+  });
+
+  return ' ' + formattedAttributes.join(' ');
 }
